56c2d573666c3d621daeeb0df7ad86b92d0a0059
DmitriyChumak <dimitriy.chumak@gmail.com>
2024-06-22 14:47:50 +0300

    initial commit

--- my_addons/mydemo/__manifest__.py
+++ my_addons/mydemo/__manifest__.py
@@ -1,1 +1,31 @@
-{}
+{
+    'name': 'mydemo',
+    'author': 'Dmitriy Chumak',
+    'website': 'http://www.dmitriy.chumak.com',
+    'version': '17.0.0.0.1',
+    'summary': 'I want to learn odoo',
+    'description': 'this module is used to learn odoo',
+    'category': 'Customization',
+    'license': 'OPL-1',
+    'depends': [
+                'base',
+                ],
+    'data': [
+        'view/smy_demo_views.xml'
+    ],
+
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'external_dependencies': {
+        'python': [''],
+    },
+
+    'images': [
+        'static/description/icon.png'
+    ],
+
+    'price': 0,
+    'currency': 'EUR',
+}
