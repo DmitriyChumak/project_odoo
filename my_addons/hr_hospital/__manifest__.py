84fee9a1cf898db61766a360168b7325fabf2008
DmitriyChumak <dimitriy.chumak@gmail.com>
2024-06-23 14:47:31 +0300

    initial commit

---
+++ my_addons/hr_hospital/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': 'Hospital management ',
+    'version': '17.0.0.0.1',
+    'summary': 'This is module should help hospital management',
+
+    'category': 'Healthcare',
+    'author': 'Dmitriy Chumak',
+    'license': 'AGPL-3',
+
+    'depends': ['base'],
+
+    'data': [
+        ''
+    ],
+
+    'demo': [
+        ''
+    ],
+
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+    'images': [
+        ''
+    ],
+
+    'price': 0,
+    'currency': 'EUR',
+}
